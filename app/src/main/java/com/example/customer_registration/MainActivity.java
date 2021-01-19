88dc21abddbc52bf30de04efc8c9add54c3db0cb
Crypto <ispa198@gmail.com>
2021-01-19 11:02:10 +0530

    2st Commit

--- app/src/main/java/com/example/customer_registration/MainActivity.java
+++ app/src/main/java/com/example/customer_registration/MainActivity.java
@@ -12,8 +12,10 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity  implements  AdapterView.OnItemSelectedListener {
-    String[] country = {"Customer Type", "USA", "China", "Japan", "Other"};
-    String[] custclass = {"Customer Class", "Normal","NRI"};
+    String[] country = {"Customer Type"};
+    String[] custclass = {"Customer Class"};
+    String[] state = {"Gujarat"};
+    String[] city = {"Vadoadara"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +40,27 @@ public class MainActivity extends AppCompatActivity  implements  AdapterView.OnI
     void spinnerinit () {
         Spinner spinnercusttype = findViewById(R.id.spinnercusttype);
         Spinner spinnercustclass = findViewById(R.id.spinnercustclass);
+        Spinner spinnerstate = findViewById(R.id.spinnestate);
+        Spinner spinnercity = findViewById(R.id.spinnercity);
 
         spinnercusttype.setOnItemSelectedListener(this);
         spinnercustclass.setOnItemSelectedListener(this);
+        spinnerstate.setOnItemSelectedListener(this);
+        spinnercity.setOnItemSelectedListener(this);
 
         ArrayAdapter spinnercustt = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,country);
         ArrayAdapter spinnercustc = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,custclass);
+        ArrayAdapter spinnerst = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,state);
+        ArrayAdapter spinnerct = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,city);
 
         spinnercustt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnercustc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerst.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerct.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinnercusttype.setAdapter(spinnercustt);
         spinnercustclass.setAdapter(spinnercustc);
+        spinnerstate.setAdapter(spinnerst);
+        spinnercity.setAdapter(spinnerct);
     }
 }
